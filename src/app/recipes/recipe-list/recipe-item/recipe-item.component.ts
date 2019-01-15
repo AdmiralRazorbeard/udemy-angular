72399f51c0a669624a3cd181bdec161d355ef95d
Thomas Fournet <t.fournet062@gmail.com>
2019-01-15 17:51:19 +0100

    new commit

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { Recipe } from "../../recipe.model";
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  selector: "app-recipe-item",
+  templateUrl: "./recipe-item.component.html",
+  styleUrls: ["./recipe-item.component.css"]
 })
 export class RecipeItemComponent implements OnInit {
+  // Input is used here to get data from outside the component
+  @Input() recipe: Recipe;
+  // Output is used to listen to this event from outside
+  @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
-
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 // Import the model
-import { Recipe } from '../recipe.model';
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  // Output is used to listen to this event from outside
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
-    new Recipe('A test Recipe', 'This is a test', 'https://dish-environment-prod-contentbucket-10u8bszryovz3.s3.amazonaws.com/assets/s3fs-public/styles/content_image_x_large/public/705316_Pumpkin-Pancakes_17036_Dianne.jpg?itok=aAJNFfet')
+    new Recipe(
+      "A test Recipe",
+      "This is a test",
+      "https://dish-environment-prod-contentbucket-10u8bszryovz3.s3.amazonaws.com/assets/s3fs-public/styles/content_image_x_large/public/705316_Pumpkin-Pancakes_17036_Dianne.jpg?itok=aAJNFfet"
+    ),
+    new Recipe(
+      "Another Recipe",
+      "This is another test",
+      "https://dish-environment-prod-contentbucket-10u8bszryovz3.s3.amazonaws.com/assets/s3fs-public/styles/content_image_x_large/public/705316_Pumpkin-Pancakes_17036_Dianne.jpg?itok=aAJNFfet"
+    )
   ];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
 }
